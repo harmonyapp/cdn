021f0bfd6701632bc8dfd0e5e08ae1fda797be95
chris9740 <33673505+chris9740@users.noreply.github.com>
2023-03-09 16:52:49 +0100

    chore: make the process respect stop signals
    This was a problem when you were running the app with docker. It would not listen to CTRL C, so you had to use docker cli to kill the container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import app from "./app.js";
 import { PORT } from "./constants.js";
 
+function signalListener() {
+    process.exit();
+}
+
+process.on("SIGINT", signalListener);
+process.on("SIGTERM", signalListener);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
